66eb0e3c2e127c59b0cebacdf44504846f6723d5
Whdan99 <99096013+Whdan99@users.noreply.github.com>
2022-02-05 22:10:06 +0200

    Add files via upload

---
+++ names menu/names menu/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace names_menu
+{
+    public class person
+    {
+        /// <summary>       
+
+
+        /// Get Or Set Person Name
+        /// </summary>
+        public string Name { get; set; }
+           
+            /// <summary>
+            /// get phone number
+            /// </summary>
+            public string PhoneNumber { get; set; }
+
+        
+        public string getName()
+        {
+            return "Person Name:"+this.Name + @"
+Person Phonenumber:"+ this.PhoneNumber;
+        }
+
+
+
+        public string getNumber()
+        {
+            return "Person Name:" + this.Name + @"
+Person Phonenumber:" + this.PhoneNumber;
+        }
+
+
+
+        public string information
+            {
+                get
+                {
+                    return $@"person
+               Name: {Name }
+              
+               Phone Number : {PhoneNumber }";
+                }
+            }
+
+            public override string ToString()
+            {
+                return information;
+            }
+
+       
+    }
+    }
+
+        
+    
---
+++ names menu/names menu/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace names_menu
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (; ; ) //Menu 
+            {
+                Console.WriteLine(@"Welcome to name menu progrem please choose option :
+1..... add person and his  number. 
+2..... delete person and his number.
+3..... show persons information list .
+4..... find person by his name .
+5..... find person by his number.
+6..... Quit the app..");
+
+                string option = Console.ReadLine();
+                option = option.Trim().ToLower();
+
+                if (option == "1")
+                    AddPersonInformations();
+
+
+                else if (option == "2")
+                    DeletePersonInformations();
+
+
+                else if (option == "3")
+
+                    ShowPersonsList();
+
+                else if (option == "4")
+                    FindPersonByHisName();
+                else if (option == "5")
+                    FindPersonByHisNumber();
+
+
+                else if (option == "Quit")
+                    break;
+
+
+
+                else Console.WriteLine("you enterd a invaild number......");
+
+
+
+                Console.WriteLine("progrem is finshed press enter to return.....");
+                Console.ReadLine();
+
+            }
+            
+
+
+        }
+        /// <summary>
+        /// (This Persons List)
+        /// </summary>
+
+        public static List<person> Persons = new List<person>();
+
+        /// <summary>
+        /// Options 
+        /// </summary>
+       
+
+
+        //Option To Add Information of Person
+        static void AddPersonInformations() 
+        {
+                
+
+                Console.WriteLine("add person name");
+                string PersonName = Console.ReadLine();
+
+
+                Console.WriteLine("add person number");
+                string PersonNumber = Console.ReadLine();
+               
+
+                Persons.Add(new person
+                {
+                    Name = $"{PersonName }",
+
+                    PhoneNumber = $"{PersonNumber }"
+                });
+
+
+
+            
+
+        }
+/// <summary>
+/// Delete info
+/// </summary>
+        static void DeletePersonInformations()
+            {
+                
+                Console.WriteLine("add person number to remove");
+                string n = Console.ReadLine();
+            int.TryParse(n, out int PersonNumber);
+
+
+
+            Persons.Remove(Persons[PersonNumber]);
+               
+            }
+
+        //Option To show Information of Persons
+
+        static void ShowPersonsList()
+            {
+
+
+
+                for (int i = 0; i < Persons.Count; i++)
+                {
+                    Console.WriteLine($"{i} => ${Persons[i]}");
+
+                }
+            }
+/// <summary>
+/// Find by Name
+/// </summary>
+        static void FindPersonByHisName()
+        {
+           
+            string Search;
+            
+
+            Console.WriteLine("Who do you want to find?");
+            Search = Console.ReadLine();
+
+            for (int iCount = 0; iCount  < Persons.Count; iCount++)
+            {
+                if (Persons[iCount].Name.Equals(Search))
+
+                {
+                    Console.WriteLine("Found it");
+                    Console.WriteLine(Persons[iCount].getName());
+
+                }
+            }
+            
+
+        }
+        /// <summary>
+        /// Find By number
+        /// </summary>
+        static void FindPersonByHisNumber()
+        {
+
+            string Search;
+
+
+            Console.WriteLine("What number do you want to find?");
+            Search = Console.ReadLine();
+
+            for (int iCount = 0; iCount < Persons.Count; iCount++)
+            {
+                if (Persons[iCount].PhoneNumber.Equals(Search))
+
+                {
+                    Console.WriteLine("Found it");
+                    Console.WriteLine(Persons[iCount].getNumber());
+
+                }
+            }
+        }
+    }           
+}
